f4a24e08ae06fa697289044fc7b8a46248f12d0f
kangmin <kangmin5505@naver.com>
2021-10-20 15:51:43 +0900

    ~10.4 Extending Templates part Two and Includes

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import models
+
 
 # Create your views here.
+def all_rooms(request):
+    rooms = models.Room.objects.all()
+    return render(request, "all_rooms.html", context={"rooms": rooms})
