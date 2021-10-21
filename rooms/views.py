6ed720ad71ef58d8e7f1184a223216b3bef0fd21
kangmin <kangmin5505@naver.com>
2021-10-21 13:57:04 +0900

    ~11.8 Class Based Views part Two

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from datetime import datetime
+from django.views.generic import ListView
 from . import models
 
 
-# Create your views here.
-def all_rooms(request):
-    rooms = models.Room.objects.all()
-    return render(request, "all_rooms.html", context={"rooms": rooms})
+class HomeView(ListView):
+
+    """HomeView Definition"""
+
+    model = models.Room
+    context_object_name = "rooms"
+    paginate_by = 10
+    paginate_orphans = 9
+
+    def get_context_data(self, **context):
+        now = datetime.now()
+        context["now"] = now
+        return super().get_context_data(**context)
