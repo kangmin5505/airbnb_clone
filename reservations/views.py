ff9bedf1bc92db741eb8239cb6647b2f199c162a
kangmin <kangmin5505@naver.com>
2021-11-09 17:45:32 +0900

    ~25.10 Conclusions/

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+import datetime
+from django.http import Http404
+from django.views.generic import View
+from django.contrib import messages
+from django.shortcuts import render, redirect, reverse
+from rooms import models as room_models
+from reviews import forms as review_forms
+from . import models as reservation_models
 
-# Create your views here.
+
+class CreateError(Exception):
+    pass
+
+
+def create(request, room, year, month, day):
+    try:
+        date_obj = datetime.datetime(year, month, day)
+        room = room_models.Room.objects.get(pk=room)
+        reservation_models.BookedDay.objects.get(day=date_obj, reservation__room=room)
+        raise CreateError()
+    except (room_models.Room.DoesNotExist, CreateError):
+        messages.error(request, "Can't Reserve That Room")
+        return redirect(reverse("core:home"))
+    except reservation_models.BookedDay.DoesNotExist:
+        reservation = reservation_models.Reservation.objects.create(
+            guest=request.user,
+            room=room,
+            check_in=date_obj,
+            check_out=date_obj + datetime.timedelta(days=1),
+        )
+        return redirect(reverse("reservations:detail", kwargs={"pk": reservation.pk}))
+
+
+class ReservationDetailView(View):
+    def get(self, *args, **kwargs):
+        pk = kwargs.get("pk")
+        reservation = reservation_models.Reservation.objects.get_or_none(pk=pk)
+        if not reservation or (
+            reservation.guest != self.request.user
+            and reservation.room.host != self.request.user
+        ):
+            raise Http404()
+        form = review_forms.CreateReviewForm()
+        return render(
+            self.request,
+            "reservations/reservation_detail.html",
+            {"reservation": reservation, "form": form}
+        )
+        
+
+def edit_reservation(request, pk, verb):
+    reservation = reservation_models.Reservation.objects.get_or_none(pk=pk)
+    if not reservation or (
+        request.user != reservation.guest
+        and request.user != reservation.room.host
+    ):
+        raise Http404()
+    if verb == "confirm":
+        reservation.status = reservation_models.Reservation.STATUS_CONFIRMED
+    elif verb == "cancel":
+        reservation.status = reservation_models.Reservation.STATUS_CANCELED
+        reservation_models.BookedDay.objects.filter(reservation=reservation).delete()
+    reservation.save()
+    messages.success(request, "Reservation Updated")
+    return redirect(reverse("reservations:detail", kwargs={"pk": reservation.pk}))
--- reviews/models.py
+++ reviews/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
+from django.core.validators import MinValueValidator, MaxValueValidator
 from core import models as core_models
 
 
@@ -8,16 +8,28 @@ class Review(core_models.TimeStampedModel):
     """Review Model Definition"""
 
     review = models.TextField()
-    accuracy = models.IntegerField()
-    communication = models.IntegerField()
-    cleanliness = models.IntegerField()
-    location = models.IntegerField()
-    check_in = models.IntegerField()
-    value = models.IntegerField()
+    accuracy = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    communication = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    cleanliness = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    location = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    check_in = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    value = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
     user = models.ForeignKey(
         "users.User", related_name="reviews", on_delete=models.CASCADE
     )
-    room = models.ForeignKey("rooms.Room", related_name="reviews", on_delete=CASCADE)
+    room = models.ForeignKey("rooms.Room", related_name="reviews", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.review} - {self.user}"
@@ -34,3 +46,6 @@ class Review(core_models.TimeStampedModel):
         return round(avg, 2)
 
     rating_average.short_description = "Avg."
+    
+    class Meta:
+        ordering = ("-created",)
